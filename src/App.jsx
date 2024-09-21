fd97778cf533d8dd267a976cc2ef22686d0fc647
SaketR3 <saketr3@illinois.edu>
2024-09-20 21:55:00 -0400

    Add state selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [state, setState] = useState(0);
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        <label htmlFor='state'>What state are you in? </label>
+        <select id='state' name='state' value={state} onChange={e => setState(Number(e.target.value))}>
+            <option value={0}>AK</option>
+            <option value={1}>AL</option>
+            <option value={2}>AR</option>
+            <option value={3}>AZ</option>
+            <option value={4}>CA</option>
+            <option value={5}>CO</option>
+            <option value={6}>CT</option>
+            <option value={7}>DC</option>
+            <option value={8}>DE</option>
+            <option value={9}>FL</option>
+            <option value={10}>GA</option>
+            <option value={11}>HI</option>
+            <option value={12}>IA</option>
+            <option value={13}>ID</option>
+            <option value={14}>IL</option>
+            <option value={15}>IN</option>
+            <option value={16}>KS</option>
+            <option value={17}>KY</option>
+            <option value={18}>LA</option>
+            <option value={19}>MA</option>
+            <option value={20}>MD</option>
+            <option value={21}>ME</option>
+            <option value={22}>MI</option>
+            <option value={23}>MN</option>
+            <option value={24}>MO</option>
+            <option value={25}>MS</option>
+            <option value={26}>MT</option>
+            <option value={27}>NC</option>
+            <option value={28}>ND</option>
+            <option value={29}>NE</option>
+            <option value={30}>NH</option>
+            <option value={31}>NJ</option>
+            <option value={32}>NM</option>
+            <option value={33}>NV</option>
+            <option value={34}>NY</option>
+            <option value={35}>OH</option>
+            <option value={36}>OK</option>
+            <option value={37}>OR</option>
+            <option value={38}>PA</option>
+            <option value={39}>RI</option>
+            <option value={40}>SC</option>
+            <option value={41}>SD</option>
+            <option value={42}>TN</option>
+            <option value={43}>TX</option>
+            <option value={44}>UT</option>
+            <option value={45}>VA</option>
+            <option value={46}>VT</option>
+            <option value={47}>WA</option>
+            <option value={48}>WI</option>
+            <option value={49}>WV</option>
+            <option value={50}>WY</option>
+        </select>
+    </div>
   )
 }
 
