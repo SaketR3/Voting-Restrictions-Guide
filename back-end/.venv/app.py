7792955b2da71661c2da03dace833ec93e527c74
SaketR3 <saketr3@illinois.edu>
2024-09-21 09:56:34 -0400

    Implemented web scraping

--- back-end/.venv/app.py
+++ back-end/.venv/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from bs4 import BeautifulSoup
+import requests 
 
 app = Flask(__name__)
 CORS(app)
@@ -8,4 +10,73 @@ CORS(app)
 def message():
     # req = request.get_json()
     # data = req['data']
-    return jsonify({'message': 'Hello World!'})
+    
+    response = requests.get('https://www.lgbtmap.org/democracy_maps/state_profile/GA')
+    html = response.text
+    soup = BeautifulSoup(html, 'lxml')
+
+    registration = []
+    representation = []
+    in_person = []
+    by_mail = []
+    security = []
+    independence = []
+
+    for t in range(6):
+        table = soup.find_all('table')[t]
+
+        headers = []
+        rows = []
+        for i, row in enumerate(table.find_all('tr')):
+            if i == 0:
+                headers = [el.text.strip() for el in row.find_all('th')]
+            else:
+                row_data = []
+                for el in row.find_all('td'):
+                    checkmark = el.find('i', class_=lambda x: x and 'fa-check-circle' in x.split())
+                    xmark = el.find('i', class_='fa fa-times-circle')
+                    if checkmark:
+                        row_data.append('Checkmark')
+                    elif xmark:
+                        row_data.append('X')
+                    else:
+                        row_data.append(el.text.strip())
+                rows.append(row_data)
+        
+        bad_laws = []
+        # for i, row in enumerate(rows):
+        #     if row[0] == 'X':
+        #         bad_laws.append(i)
+        for i in range(len(rows)):
+            if rows[i] != [] and rows[i][1] == 'NEGATIVE LAW': 
+                cleaned_string = rows[i][0].replace('Negative Law:\n', '') 
+                cleaned_string = cleaned_string.replace('Negative Law: \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t \n\t              \t\t\n\t              \t\t\t              \t\t\t', '') 
+                # cleaned_string = rows[i][0].strip().replace('Negative Law:\n', '')
+                bad_laws.append(cleaned_string)
+
+        if t == 0:
+            registration = bad_laws
+        elif t == 1:
+            representation = bad_laws
+        elif t == 2:
+            in_person = bad_laws
+        elif t == 3:
+            by_mail = bad_laws
+        elif t == 4:
+            security = bad_laws
+        elif t == 5:
+            independence = bad_laws
+    
+    # print('\n\n\nRegistration: ', registration) 
+    # print('\n\n\nRepresentation: ', representation) 
+    # print('\n\n\nIn-person: ', in_person) 
+    # print('\n\n\nBy mail: ', by_mail) 
+    # print('\n\n\nSecurity: ', security) 
+    # print('\n\n\nIndependence: ', independence) 
+
+    return jsonify({'registration': registration},
+                    {'representation': representation},
+                    {'in_person': in_person},
+                    {'by_mail': by_mail},
+                    {'security': security},
+                    {'independence': independence})
